43e23739552285236540985aa1d4cf6f422d9846
“ShayanKh76” <shayan.khalighi@studenti.polito.it>
2024-11-02 11:14:19 +0100

    feat: login page

--- code/client/src/API/API.ts
+++ code/client/src/API/API.ts
@@ -1,4 +1,21 @@
 const SERVER_URL = "http://localhost:3001/api";
 
-const API = {};
+const login = async (username: string, password: string) => {
+  const response = await fetch(`${SERVER_URL}/sessions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Login failed");
+  }
+
+  return await response.json();
+};
+
+const API = { login };
 export default API;
