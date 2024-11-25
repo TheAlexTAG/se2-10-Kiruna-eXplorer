449476ed80bb50c25d0fb9db2253bc48ac5bce78
Marco Russo s329270 <s329270@studenti.polito.it>
2024-11-25 12:38:29 +0100

    rearranged all documents for maraiDB; Created Backup folder with controllers, DAOs, routes and index.ts

---
+++ backup/dao/link_docDAO.ts
@@ -0,0 +1,86 @@
+import { LinkDocument } from "../components/link_doc";
+import db from "../db/db";
+import { DocumentNotFoundError } from "../errors/documentErrors";
+import { InternalServerError, LinkError } from "../errors/link_docError";
+
+/* Sanitize input */
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
+
+class LinkDocumentDAO {
+
+  getLink(firstDoc: number, secondDoc: number): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      const sql = "SELECT * FROM link WHERE (firstDoc=? AND secondDoc=?) OR (firstDoc=? AND secondDoc=?)";
+      db.get(sql, [firstDoc, secondDoc, secondDoc, firstDoc], (err: Error | null, row: any) => {
+        if (err) {
+          return reject(new InternalServerError(err.message));
+        }
+        if (!row) {
+          return resolve(null);
+        }
+        return resolve(new LinkDocument(+DOMPurify.sanitize(row.firstDoc), +DOMPurify.sanitize(row.secondDoc), DOMPurify.sanitize(row.relationship)));
+      }
+      );
+    }
+    );
+  };
+
+  getDocumentConnections(documentID: number): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      const sql = "SELECT count(*) as tot FROM link WHERE firstDoc=? OR secondDoc=?";
+      db.get(sql, [documentID, documentID], (err: Error | null, row: any) => {
+        if (err) {
+          return reject(new InternalServerError(err.message));
+        }
+        if (!row.tot) {
+          return reject(new LinkError());
+        }
+        return resolve(+DOMPurify.sanitize(row.tot));
+      }
+      );
+    }
+    );
+  };
+
+  checkDocuments(first: number, secondDoc: number): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      const sql = "SELECT count(*) as tot FROM document WHERE documentID=? OR documentID=?";
+      db.get(sql, [first, secondDoc], (err: Error | null, row: any) => {
+        if (err) {
+          return reject(new InternalServerError(err.message));
+        }
+        const spy: number = +DOMPurify.sanitize(row.tot);
+
+        if (spy < 2) {
+          return resolve(false);
+        }
+        return resolve(true);
+      }
+      );
+    }
+    );
+  };
+
+  insertLink(links: LinkDocument[]): Promise<boolean> {
+    return new Promise<boolean>(function (resolve, reject) {
+      const placeholders = links.map(() => "(?,?,?)").join();
+      const sql = `insert into link(firstDoc, secondDoc, relationship) VALUES ${placeholders}`;
+      const values= links.flatMap(link=> [link.firstDoc, link.secondDoc, link.relationship]);
+
+      db.run(sql, values, async function (err: Error | null) {
+        if (err) {
+          return reject(new InternalServerError(err.message));
+        }
+        return resolve(true);
+      }
+     );
+    }
+   );
+  };
+
+}
+
+export {LinkDocumentDAO};
--- backup/index.ts
+++ backup/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { testConnection } from "./src/db/db";
+import {DocumentRoutes} from "./src/routers/documentRoutes"
+import {UserController} from "./src/controllers/userController";
+import {UserRoutes} from "./src/routers/userRoutes";
+import {LinkDocumentRoutes} from "./src/routers/link_docRoutes";
+import {ZoneRoutes} from "./src/routers/zoneRoutes";
+import db from "./src/db/db";
 import { Kiruna } from "./src/helper";
-import { Role, User } from "./src/components/user";
 
 
 const morgan = require("morgan"); // logging middleware
@@ -33,12 +37,12 @@ app.use('/resources', express.static(resourceDir));
 }
 
 /*** Passport ***/
-/** provisorial user for the login */
-const user = new User(1, 'username', Role.PLANNER);
+const controller = new UserController();
 
 // set up the "username and password" login strategy with a function to verify username and password
 passport.use(
   new LocalStrategy(async function verify(username: string,password: string,callback: any){
+    const user = await controller.getUser(username, password);
     if (!user) {
       return callback(null, false, {message: "Incorrect username or password"});
     }
@@ -52,6 +56,7 @@ passport.serializeUser((user: any, callback: any) => {
 
 passport.deserializeUser(async function (id: number, callback: any) {
   try {
+    const user = await controller.getUserById(id);
     callback(null, user);
   } catch (err) {
     callback(err, null);
@@ -78,29 +83,20 @@ app.use(passport.session());
 
 /* ROUTES */
 
+new DocumentRoutes(app);
+new UserRoutes(app, passport, isLoggedIn);
+new LinkDocumentRoutes(app);
+new ZoneRoutes(app);
+const kiruna= new Kiruna();
+kiruna.checkKiruna().catch((err: any)=>{
+  console.error(`Error code:${err.code}\nMessage:${err.message}`);
+  db.close();
+});;
 
 /*** Other express-related instructions ***/
 // activate the server
 const server= app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
-
-testConnection();
-/*
-  example query with mariaDB, be sure to always open the connection and close it in finally
-  app.get("/api/zones", async function(req: any, res: any, next: any) {
-    let conn;
-    try {
-      conn = await db.pool.getConnection();
-      let result = await conn.query("SELECT * FROM zone");
-      console.log(result);
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(400).json(err);
-    } finally {
-      if(conn) conn.release();
-    }
-  })
-*/
 
 export {app, server};
