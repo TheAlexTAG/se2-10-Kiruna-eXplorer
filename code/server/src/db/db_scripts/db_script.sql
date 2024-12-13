87e7d87d9da7c30fac73a2a75dd91fa7398f6fc4
Marco Russo s329270 <s329270@studenti.polito.it>
2024-12-13 09:47:00 +0100

    fix: db_script.ts now has no constraint on relationship type of connections; fix: now InternalServerError covers sensitive informations

--- code/server/src/db/db_scripts/db_script.sql
+++ code/server/src/db/db_scripts/db_script.sql
@@ -37,7 +37,7 @@ CREATE TABLE link (
 	firstDoc INT NOT NULL,
 	secondDoc INT NOT NULL,
 	relationship ENUM('Direct consequence', 'Collateral consequence', 'Projection', 'Update'),
-	PRIMARY KEY (linkID, relationship),
+	PRIMARY KEY (linkID),
 	FOREIGN KEY (firstDoc) REFERENCES document(documentID) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (secondDoc) REFERENCES document(documentID) ON UPDATE CASCADE ON DELETE CASCADE
 );
